6b299deb3bfff3fbd29bfee4e7ec8388b3edf288
OreoJuice <114830718+OreoJuice@users.noreply.github.com>
2023-05-16 03:19:03 -0400

    updates
    added more input dialogue, added defs to program, program has the option to extract again on finish

--- extract.py
+++ extract.py
@@ -5,130 +5,159 @@ import shutil
 import pandas as pd
 import sys
 import threading
+import time
 
-# Input for which character to extract dialogue for
-inputCharacter = input('Enter character name: ')
+def continueQuestion():
+    continueProgram = input("Would you like to extract again? [Y/N]: ")
+    if continueProgram.upper() == "Y":
+        print("...Running Program Again...")
+        main()
+    if continueProgram.upper() == "N":
+        print("...Exiting Program...")
+        sys.exit()
+    else:
+        print("...Invalid input...\n")
+        continueQuestion()
 
-# Make directories for output
-pathOutputInput = './Output/InputLines'
-pathOutputCharacter = './Output/CharacterLines'
+def extract(x):
+    # Input for which character to extract dialogue for
+    inputCharacter = x
 
-try:
-    os.makedirs(pathOutputInput)
-    os.makedirs(pathOutputCharacter)
-except FileExistsError:
-    print()
+    # Make directories for output and start timer
+    startTime = time.perf_counter()
 
-success = False
+    pathOutputInput = './Output/InputLines'
+    pathOutputCharacter = './Output/CharacterLines'
 
-# path to text files to be read
-dirPath = r'Input\**\*.txt'
-savePath = r'Output\InputLines'
+    try:
+        os.makedirs(pathOutputInput)
+        os.makedirs(pathOutputCharacter)
+    except FileExistsError:
+        print()
 
-# This will list all text files in the input folder, only use for debugging
-"""for file in glob.glob(dirPath, recursive=True):
-    print(file)"""
+    success = False
 
-# Read each text file in input folder and write each file to one big combined text file
-inputFolder = glob.glob(dirPath, recursive=True)
-mergedInput = open("merged.txt", "w+", encoding='utf8')
+    # path to text files to be read
+    dirPath = r'Input\**\*.txt'
+    #savePath = r'Output\InputLines'
 
-for file in inputFolder:
-    with open('{}'.format(file), 'r+', encoding='utf8') as input:
-        mergedInput.write(input.read())
+    # This will list all text files in the input folder, only use for debugging
+    """for file in glob.glob(dirPath, recursive=True):
+        print(file)"""
 
-mergedInput.close()
+    # Read each text file in input folder and write each file to one big combined text file
+    inputFolder = glob.glob(dirPath, recursive=True)
+    mergedInput = open("merged.txt", "w+", encoding='utf8')
 
-# Move the combined text file to output folder
-sourceFolder = r'merged.txt'
-destinationFolder = r'Output/InputLines/merged.txt'
-shutil.move(sourceFolder, destinationFolder)
+    for file in inputFolder:
+        with open('{}'.format(file), 'r+', encoding='utf8') as input:
+            mergedInput.write(input.read())
 
-# Open merged file to read
-allTextPath = r'Output/InputLines/merged.txt'
-allText = open(allTextPath, 'r+', encoding='utf8')
+    mergedInput.close()
 
-# Ascii handling, replace � with a space and move to output folder
-fixedMerged = open('fixedMerged.txt', 'w+', encoding='utf8')
+    # Move the combined text file to output folder
+    sourceFolder = r'merged.txt'
+    destinationFolder = r'Output/InputLines/merged.txt'
+    shutil.move(sourceFolder, destinationFolder)
 
-for line in allText:
-    line.replace('�', ' ')
-    fixedMerged.write(line)
+    # Open merged file to read
+    allTextPath = r'Output/InputLines/merged.txt'
+    allText = open(allTextPath, 'r+', encoding='utf8')
 
-fixedMerged.close()
+    # Ascii handling, replace � with a space and move to output folder
+    fixedMerged = open('fixedMerged.txt', 'w+', encoding='utf8')
 
-fixedMergedSourceFolder = r'fixedMerged.txt'
-fixedMergedDestFolder = r'Output/Inputlines/fixedMerged.txt'
-shutil.move(fixedMergedSourceFolder, fixedMergedDestFolder)
+    for line in allText:
+        line.replace('�', ' ')
+        fixedMerged.write(line)
 
-# Extract all character dialogue and put into a text file and into a list
-fixedMergedPath = r'Output/InputLines/fixedMerged.txt'
-readFixedMerged = open(fixedMergedPath, 'r+', encoding='utf8')
-characterDialogue = open('{}_Dialogue.txt'.format(inputCharacter), 'w+', encoding='utf8')
-characterLines = []
+    fixedMerged.close()
 
-characterCheck = ':'
+    fixedMergedSourceFolder = r'fixedMerged.txt'
+    fixedMergedDestFolder = r'Output/Inputlines/fixedMerged.txt'
+    shutil.move(fixedMergedSourceFolder, fixedMergedDestFolder)
 
-# Threadlocking so we don't have a race condition error
+    # Extract all character dialogue and put into a text file and into a list
+    fixedMergedPath = r'Output/InputLines/fixedMerged.txt'
+    readFixedMerged = open(fixedMergedPath, 'r+', encoding='utf8')
+    characterDialogue = open('{}_Dialogue.txt'.format(inputCharacter), 'w+', encoding='utf8')
+    characterLines = []
 
-lock = threading.Lock()
-lock.acquire()
+    characterCheck = ':'
 
-for line in readFixedMerged:
-    if line.startswith('{}:'.format(inputCharacter)):
-        index = line.find(characterCheck)
-        characterDialogue.write(line[index + 2:])
-        characterLines.append(line[index + 2:].rstrip('\n'))
-        #ak12Dialogue.write(f"{line}\n")
+    # Threadlocking so we don't have a race condition error
+
+    lock = threading.Lock()
+    lock.acquire()
+
+    for line in readFixedMerged:
+        if line.startswith('{}:'.format(inputCharacter)):
+            index = line.find(characterCheck)
+            characterDialogue.write(line[index + 2:])
+            characterLines.append(line[index + 2:].rstrip('\n'))
+            #ak12Dialogue.write(f"{line}\n")
+            
+    characterDialogue.close()
+    lock.release()
+
+    # Reading all lines in characterLines for debugging
+    """for lines in characterLines:
+        print(lines)"""
+
+    # This shit is the reason why some characters did not work for extraction
+    # Commenting until I can fix it (May 13 2023)
+    # May 15 2023 - Fixed it
+    # Possible explanation after sleeping on it: Race condition
+    # Check if text file is empty, if yes delete file and exit program
+    if os.path.getsize('{}_Dialogue.txt'.format(inputCharacter)) == 0:
+        print('{} does not exist in story files. Dialouge file is empty. Check if input is typed correctly\n'.format(inputCharacter))
+        os.remove('{}_Dialogue.txt'.format(inputCharacter))
+        continueQuestion()
+
+    # Convert lines in list to csv file
+    characterCsv = open('{}_Dialogue.csv'.format(inputCharacter), 'w+', encoding='utf8')
+
+    dataFrame = pd.DataFrame(data = characterLines)
+    dataFrame.to_csv('{}_Dialogue.csv'.format(inputCharacter), header=False, index=False, encoding='utf8')
+
+    # Close files so we can move them into folders
+    fixedMerged.close()
+    readFixedMerged.close()
+    characterDialogue.close()
+    characterCsv.close()
+    mergedInput.close()
+    allText.close()
+
+    # Move character_Dialogue.txt, and character_Dialogue.csv to their respective folders
+    characterDialogueTxtSourceFolder = r'{}_Dialogue.txt'.format(inputCharacter)
+    characterDialogueCsvSourceFolder = r'{}_Dialogue.csv'.format(inputCharacter)
+
+    characterDialogueTxtDestFolder = r'Output/CharacterLines/{}_Dialogue.txt'.format(inputCharacter)
+    characterDialogueCsvDestFolder = r'Output/CharacterLines/{}_Dialogue.csv'.format(inputCharacter)
+
+    shutil.move(characterDialogueTxtSourceFolder, characterDialogueTxtDestFolder)
+    shutil.move(characterDialogueCsvSourceFolder, characterDialogueCsvDestFolder)
+
+    success=True
+
+    # If success, print message to console, else print failure
+    if success==True:
+        print("Extract Success. Check Output folder.\n")
+
+    if success==False:
+        print('Extract Failure.\n')
         
-characterDialogue.close()
-lock.release()
+    # Print out total time program takes to execute
+    endTime = time.perf_counter()
+    elapsedTime = str(endTime - startTime)
+    print(elapsedTime, 'seconds to extract.\n')
 
-# Reading all lines in characterLines for debugging
-"""for lines in characterLines:
-    print(lines)"""
+    continueQuestion()
 
-# This shit is the reason why some characters did not work for extraction
-# Commenting until I can fix it (May 13 2023)
-# May 15 2023 - Fixed it
-# Explanation after sleeping on it: Race condition
-# Check if text file is empty, if yes delete file and exit program
-if os.path.getsize('{}_Dialogue.txt'.format(inputCharacter)) == 0:
-    print('Character does not exist. File is empty. Check if input is typed correctly')
-    os.remove('{}_Dialogue.txt'.format(inputCharacter))
-    sys.exit()
+def main():
+    # Input for which character to extract dialogue for
+    inputCharacter = input('\nEnter character name: ')
+    extract(inputCharacter)
 
-# Convert lines in list to csv file
-characterCsv = open('{}_Dialogue.csv'.format(inputCharacter), 'w+', encoding='utf8')
-
-dataFrame = pd.DataFrame(data = characterLines)
-dataFrame.to_csv('{}_Dialogue.csv'.format(inputCharacter), header=False, index=False, encoding='utf8')
-
-# Close files so we can move them into folders
-fixedMerged.close()
-readFixedMerged.close()
-characterDialogue.close()
-characterCsv.close()
-mergedInput.close()
-allText.close()
-
-# Move character_Dialogue.txt, and character_Dialogue.csv to their respective folders
-characterDialogueTxtSourceFolder = r'{}_Dialogue.txt'.format(inputCharacter)
-characterDialogueCsvSourceFolder = r'{}_Dialogue.csv'.format(inputCharacter)
-
-characterDialogueTxtDestFolder = r'Output/CharacterLines/{}_Dialogue.txt'.format(inputCharacter)
-characterDialogueCsvDestFolder = r'Output/CharacterLines/{}_Dialogue.csv'.format(inputCharacter)
-
-shutil.move(characterDialogueTxtSourceFolder, characterDialogueTxtDestFolder)
-shutil.move(characterDialogueCsvSourceFolder, characterDialogueCsvDestFolder)
-
-success=True
-
-# If success, print message to console, else print failure
-if success==True:
-    print("Extract Success. Check Output folder.")
-
-if success==False:
-    print('Extract Failure.')
-
-sys.exit()
+if __name__ == "__main__":
+    main()
