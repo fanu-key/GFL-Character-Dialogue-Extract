04bb06277e63076a494941c62b3d9fa4aebae98c
fanukey <165209848+fanu-key@users.noreply.github.com>
2025-03-25 17:11:21 +0300

    nothing, so small update

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ config = Config()
 config.bind = ["0.0.0.0:5000"]
 if __name__ == "__main__":
     print("-------------------------------")
-    print("\n\n Commander, open the site http://localhost:5000/ or you can open extractCLI.py ​​if you are sick of sites. \n\n")
+    print("\n\n Commander, open the site http://localhost:5000/ or you can open extractCLI.py if you are sick of sites. \n\n")
     print("-------------------------------")
     asyncio.run(serve(app, config))
