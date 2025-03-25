0e58ac002f5cd46ad02b717dcab22baa64561f32
fanukey <165209848+fanu-key@users.noreply.github.com>
2025-03-25 15:41:24 +0300

    Added a basic-template html/css

--- api.py
+++ api.py
@@ -4,8 +4,19 @@ from config import *
 app = Quart(__name__)
 
 @app.route('/')
-async def hello():
-    return 'hello'
+async def home():
+    return await render_template('home.html')
+
+@app.route('/extract', methods=['POST'])
+async def extract():
+    form = await request.form
+
+    character_name = form.get('character')
+    if not character_name:
+        return redirect(url_for('home'))
+    
+    result = "Okay, puppy, just wait..."
+    return result
 
 # If you see this, don't pay attention, I just write code and it goes to the repository. Check back later!
 app.run()
--- imports.py
+++ imports.py
@@ -4,4 +4,4 @@ import time
 import asyncio
 import csv
 from concurrent.futures import ThreadPoolExecutor
-from quart import Quart
+from quart import Quart, render_template, request, redirect, url_for
